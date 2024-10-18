1065bfce9b686e930f54562ec6cd8430079d02c2
eunhye <masimel33@gmail.com>
2024-10-18 17:25:37 +0900

    upload

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,173 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+        <ul class="header-button-left">
+            <li>Cancel</li>
+        </ul>
+        <ul class="header-button-right">
+            <li v-if="step === 1" @click="step++">Next</li>
+            <li v-if="step === 2" @click="publish">발행</li>
+        </ul>
+        <img src="./assets/logo.png" class="logo" />
+    </div>
+
+    <container :posts="Posts" :step="step" :imgUrl="imgUrl" @write="writeContent = $event" />
+    <button @click="more" class="more">더보기</button>
+
+    <div class="footer">
+        <ul class="footer-button-plus">
+            <input @change="upload" type="file" id="file" class="inputfile" />
+            <label for="file" class="input-plus">+</label>
+        </ul>
+    </div>
+    <!-- 
+    <div v-if="step === 0">내용1</div>
+    <div v-if="step === 1">내용2</div>
+    <div v-if="step === 2">내용3</div>
+
+    <button @click="step = 0">버튼1</button>
+    <button @click="step = 1">버튼2</button>
+    <button @click="step = 2">버튼3</button> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from "./components/Container.vue";
+import posts from "./assets/posting.js";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            Posts: posts,
+            moreNum: 0,
+            step: 0,
+            imgUrl: "",
+            writeContent: "",
+            // selectFilter: "",
+        };
+    },
+    // mounted() {
+    //     this.emitter.on("filter", (data) => {
+    //         this.selectFilter = data;
+    //     });
+    // },
+    methods: {
+        more() {
+            axios.get(`https://codingapple1.github.io/vue/more${this.moreNum}.json`).then((result) => {
+                this.Posts.push(result.data);
+                this.moreNum++;
+            });
+        },
+        upload(e) {
+            let file = e.target.files[0];
+            this.step++;
+            let url = URL.createObjectURL(file);
+            this.imgUrl = url;
+        },
+        publish() {
+            let data = {
+                name: "Kim Hyun",
+                userImage: "https://picsum.photos/100?random=3",
+                postImage: this.imgUrl,
+                likes: 36,
+                date: "May 15",
+                liked: false,
+                content: this.writeContent,
+                filter: "perpetua",
+            };
+            this.Posts.unshift(data);
+            this.step = 0;
+        },
+    },
+    components: {
+        Container,
+    },
+};
 </script>
 
 <style>
+body {
+    margin: 0;
+}
+ul {
+    padding: 5px;
+    list-style-type: none;
+}
+.logo {
+    width: 22px;
+    margin: auto;
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 13px;
+}
+.header {
+    width: 100%;
+    height: 40px;
+    background-color: white;
+    padding-bottom: 8px;
+    position: sticky;
+    top: 0;
+}
+.header-button-left {
+    color: skyblue;
+    float: left;
+    width: 50px;
+    padding-left: 20px;
+    cursor: pointer;
+    margin-top: 10px;
+}
+.header-button-right {
+    color: skyblue;
+    float: right;
+    width: 50px;
+    cursor: pointer;
+    margin-top: 10px;
+}
+.footer {
+    width: 100%;
+    position: sticky;
+    bottom: 0;
+    padding-bottom: 10px;
+    background-color: white;
+}
+.footer-button-plus {
+    width: 80px;
+    margin: auto;
+    text-align: center;
+    cursor: pointer;
+    font-size: 24px;
+    padding-top: 12px;
+}
+.sample-box {
+    width: 100%;
+    height: 600px;
+    background-color: bisque;
+}
+.inputfile {
+    display: none;
+}
+.input-plus {
+    cursor: pointer;
+}
+.more {
+    border: none;
+    width: 100%;
+    height: 45px;
+    font-size: 16px;
+    font-weight: 500;
+    background: #ddd;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    box-sizing: border-box;
+    font-family: "consolas";
+    margin-top: 60px;
+    width: 100%;
+    max-width: 460px;
+    margin: auto;
+    position: relative;
+    border-right: 1px solid #eee;
+    border-left: 1px solid #eee;
 }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,122 @@
+<template>
+    <div v-if="step === 0"><Post :post="Posts[idx]" v-for="(post, idx) in posts" :key="post" /></div>
+
+    <!-- 필터선택페이지 -->
+    <div v-if="step === 1">
+        <div class="upload-image" :class="selectFilter" :style="{ backgroundImage: `url(${imgUrl})` }"></div>
+        <div class="filters">
+            <FilterBox :imgUrl="imgUrl" v-for="filter in filters" :key="filter" :filter="filter">{{ filter }}</FilterBox>
+        </div>
+    </div>
+
+    <!-- 글작성페이지 -->
+    <div v-if="step === 2">
+        <div class="upload-image" :style="{ backgroundImage: `url(${imgUrl})` }"></div>
+        <div class="write">
+            <textarea @input="$emit('write', $event.target.value)" class="write-box">write!</textarea>
+        </div>
+    </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+import posts from "../assets/posting.js";
+import FilterBox from "./FilterBox.vue";
+
+export default {
+    data() {
+        return {
+            Posts: posts,
+            filters: [
+                "aden",
+                "_1977",
+                "brannan",
+                "brooklyn",
+                "clarendon",
+                "earlybird",
+                "gingham",
+                "hudson",
+                "inkwell",
+                "kelvin",
+                "lark",
+                "lofi",
+                "maven",
+                "mayfair",
+                "moon",
+                "nashville",
+                "perpetua",
+                "reyes",
+                "rise",
+                "slumber",
+                "stinson",
+                "toaster",
+                "valencia",
+                "walden",
+                "willow",
+                "xpro2",
+            ],
+            selectFilter: "",
+        };
+    },
+    props: {
+        posts: Array,
+        step: Number,
+        imgUrl: String,
+    },
+    components: {
+        Post,
+        FilterBox,
+    },
+    mounted() {
+        this.emitter.on("filter", (data) => {
+            this.selectFilter = data;
+            console.log(this.selectFilter);
+        });
+    },
+};
+</script>
+
+<style>
+.upload-image {
+    width: 100%;
+    height: 450px;
+    background: cornflowerblue;
+    background-size: cover;
+}
+.filters {
+    overflow-x: scroll;
+    white-space: nowrap;
+}
+.filter-1 {
+    width: 100px;
+    height: 100px;
+    background-color: cornflowerblue;
+    margin: 10px 10px 10px auto;
+    padding: 8px;
+    display: inline-block;
+    color: white;
+    background-size: cover;
+}
+.filters::-webkit-scrollbar {
+    height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+    background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+    background: #555;
+}
+.write-box {
+    border: none;
+    width: 90%;
+    height: 100px;
+    padding: 15px;
+    margin: auto;
+    display: block;
+    outline: none;
+}
+</style>
